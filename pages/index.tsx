d61e9aa4c664677cbe461c540d12ab8fd0a2fea2
jmigsan <>
2022-07-20 15:25:14 +0100

    prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,29 @@
 import type { NextPage } from 'next';
-import Head from "next/head";
+import Head from 'next/head';
 
 import { Box, Center, Text } from '@chakra-ui/react';
-import NavBar from '../components/All/NavBar'
+import NavBar from '../components/All/NavBar';
 
 const Home: NextPage = () => {
   return (
     <>
-    <Head>
-      <title>Juan Miguel Sanchez</title>
-      <meta name="description" content="Juan Miguel Sanchez's Portfolio Home Page" />
-    </Head>
+      <Head>
+        <title>Juan Miguel Sanchez</title>
+        <meta
+          name='description'
+          content="Juan Miguel Sanchez's Portfolio Home Page"
+        />
+      </Head>
 
-    <NavBar/>
+      <NavBar />
 
-    <Box>
-      <Center>
-        <Text>
-          yo
-        </Text>
-      </Center>
-    </Box>
+      <Box>
+        <Center>
+          <Text>yo</Text>
+        </Center>
+      </Box>
     </>
-  )
+  );
 };
 
 export default Home;
