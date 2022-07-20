2c9e538d878cf8e04a8a30a73303d93992fa4c4e
jmigsan <>
2022-07-20 13:50:36 +0100

    theme thing no more ()

--- styles/chakra-theme.ts
+++ styles/chakra-theme.ts
@@ -2,12 +2,19 @@ import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
   styles: {
-    global: () => ({
+    global: {
       body: {
         bg: '#ecf0f1',
       }
-    })
+    }
   },
+  colors: {
+    brand: {
+      900: '#1a365d',
+      800: '#153e75',
+      700: '#2a69ac',
+    },
+  }
 });
 
 export default theme;
